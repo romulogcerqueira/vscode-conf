3a3d82a1f651b5891412bf24be6d972962ea78bb
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2018-09-20 21:25:33 -0300

    Extension install script - initial commit

---
+++ vscode-extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+code --install-extension file-icons.file-icons
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension ms-vscode.cpptools
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension twxs.cmake
+code --install-extension slevesque.shader
