160f8f6b93d6b88b42aa6ebe76922f3b60623793
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2018-09-21 00:50:53 -0300

    Add Python, PDF, Ruby extensions

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -6,3 +6,12 @@ code --install-extension ms-vscode.cpptools
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension twxs.cmake
 code --install-extension slevesque.shader
+code --install-extension batisteo.vscode-django
+code --install-extension donjayamanne.jupyter
+code --install-extension donjayamanne.python-extension-pack
+code --install-extension magicstack.MagicPython
+code --install-extension ms-python.python
+code --install-extension rebornix.ruby
+code --install-extension tomoki1207.pdf
+code --install-extension wholroyd.jinja
+code --install-extension VisualStudioExptTeam.vscodeintellicode
